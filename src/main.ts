aaecc13fbec6ac2769b03212301f646ba2bd6a13
Paul Joubert <pauljjoubert@gmail.com>
2020-07-04 21:38:33 +0200

    Adjust sensitivity

--- src/main.ts
+++ src/main.ts
@@ -106,14 +106,7 @@ function addEventListenersPointer(
                 let center = point0.add(point1).scale(0.5);
 
                 if (previousDistance > 0) {
-                    if (currentDistance > previousDistance) {
-                        // The distance between the two pointers has increased, zoom in
-                        zoom(transformation, center, 1.03)
-                    }
-                    if (currentDistance < previousDistance) {
-                        // The distance between the two pointers has decreased, zoom out
-                        zoom(transformation, center, 0.97)
-                    }
+                    zoom(transformation, center, currentDistance / previousDistance)
                 }
 
                 // Cache the distance for the next move event 
